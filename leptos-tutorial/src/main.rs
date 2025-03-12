6e3b7332523503fd9634c5f498ecf29502d8a2c9
jamestheengineer <jamestheengineer@gmail.com>
2025-03-12 19:11:58 -0400

    styles

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -5,48 +5,19 @@ use leptos::prelude::*;
 // They define a reusable unit of behavior
 #[component]
 fn App() -> impl IntoView {
-    // here we create a reactive signal
-    // and get a (getter, setter) pair
-    // signals are the basic unit of change in the framework
-    // we'll talk more about them later
     let (count, set_count) = signal(0);
 
-    // the `view` macro is how we define the user interface
-    // it uses an HTML-like format that can accept certain Rust values
     view! {
         <button
-            // on:click will run whenever the `click` event fires
-            // every event handler is defined as `on:{eventname}`
-
-            // we're able to move `set_count` into the closure
-            // because signals are Copy and 'static
-
-            on:click=move |_| *set_count.write() += 1
+            on:click=move |_| {
+                *set_count.write() += 1;
+            }
+            class:red=move || count.get() % 2 == 1
         >
-            // text nodes in RSX should be wrapped in quotes,
-            // like a normal Rust string
             "Click me: "
             {count}
+            
         </button>
-        <p>
-            <strong>"Reactive: "</strong>
-            // you can insert Rust expressions as values in the DOM
-            // by wrapping them in curly braces
-            // if you pass in a function, it will reactively update
-            {move || count.get()}
-        </p>
-        <p>
-            <strong>"Reactive shorthand: "</strong>
-            // you can use signals directly in the view, as a shorthand
-            // for a function that just wraps the getter
-            {count}
-        </p>
-        <p>
-            <strong>"Not reactive: "</strong>
-            // NOTE: if you just write {count.get()}, this will *not* be reactive
-            // it simply gets the value of count once
-            {count.get()}
-        </p>
     }
 }
 
