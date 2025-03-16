86677c43342b4a140e8e14f05d8042f95bc793a0
jamestheengineer <jamestheengineer@gmail.com>
2025-03-16 10:08:14 -0400

    more than one component and props

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,38 +1,25 @@
 use leptos::prelude::*;
 
-// The #[component] macro marks a function as a reusable component
-// Components are the building blocks of your user interface
-// They define a reusable unit of behavior
+#[component]
+fn ProgressBar(
+    progress: ReadSignal<i32>
+) -> impl IntoView {
+    view! {
+        <progress
+            max="50"
+            // now this works
+            value=progress
+        />
+    }
+}
+
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
-    let double_count = move || count.get() * 2;
-
     view! {
-        <button
-            on:click=move |_| {
-                *set_count.write() += 10;
-            }
-            // set the `style` attribute
-            style="position: absolute"
-            // and toggle individual CSS properties with `style:`
-            style:left=move || format!("{}px", count.get() + 100)
-            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
-            style:max-width="400px"
-            // Set a CSS variable for stylesheet use
-            style=("--columns", move || count.get().to_string())
-        >
-            "Click to Move"
-        </button>
-        <progress
-            max="50"
-            // we use it once here
-            value=double_count
-        />
-        <p>
-            "Double Count: " // and again here
-            {double_count}
-        </p>
+        <button on:click=move |_| *set_count.write() += 1>"Click me"</button>
+        // now we use our component!
+        <ProgressBar progress=count />
     }
 }
 
