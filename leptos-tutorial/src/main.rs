d5af240d593346023094027d76b709c9337697de
jamestheengineer <jamestheengineer@gmail.com>
2025-03-20 21:54:46 -0400

    documentation

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,8 +1,12 @@
 use leptos::prelude::*;
+
+/// Shows progress toward a goal.
 #[component]
 fn ProgressBar(
+    /// The maximum value of the progress bar.
     #[prop(default = 100)]
     max: u16,
+    /// How much progress should be displayed.
     #[prop(into)]
     progress: Signal<i32>
 ) -> impl IntoView
