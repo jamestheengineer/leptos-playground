4e5acc3e6589ed988f4244c0d4927e4c96ba1982
jamestheengineer <jamestheengineer@gmail.com>
2025-03-21 11:57:41 -0400

    static lists

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -19,16 +19,13 @@ fn ProgressBar(
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-    let double_count = move || count.get() * 2;
-
-    view! {
-        <button on:click=move |_| *set_count.write() += 1>"Click me"</button>
-        // .into() converts `ReadSignal` to `Signal`
-        <ProgressBar progress=count />
-        // use `Signal::derive()` to wrap a derived signal
-        <ProgressBar progress=Signal::derive(double_count) />
-    }
+    let values = vec![0, 1, 2];
+view! {
+    // this will just render "012"
+    <p>{values.clone()}</p>
+    // or we can wrap them in <li>
+    <ul>{values.into_iter().map(|n| view! { <li>{n}</li> }).collect_view()}</ul>
+}
 }
 
 // This `main` function is the entry point into the app
