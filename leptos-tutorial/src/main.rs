978e5f90a40a3e785d236fce633b76e4f5c69101
jamestheengineer <jamestheengineer@gmail.com>
2025-03-24 18:20:21 -0400

    make it work

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[derive(Debug, Clone)]
 struct DatabaseEntry {
     key: String,
-    value: i32,
+    value: RwSignal<i32>,
 }
 
 #[component]
@@ -12,26 +12,24 @@ pub fn App() -> impl IntoView {
     let (data, set_data) = signal(vec![
         DatabaseEntry {
             key: "foo".to_string(),
-            value: 10,
+            value: RwSignal::new(10),
         },
         DatabaseEntry {
             key: "bar".to_string(),
-            value: 20,
+            value: RwSignal::new(20),
         },
         DatabaseEntry {
             key: "baz".to_string(),
-            value: 15,
+            value: RwSignal::new(15),
         },
     ]);
     view! {
         // when we click, update each row,
         // doubling its value
         <button on:click=move |_| {
-            set_data.update(|data| {
-                for row in data {
-                    row.value *= 2;
-                }
-            });
+            for row in &*data.read() {
+                row.value.update(|value| *value *= 2);
+            }
             // log the new value of the signal
             leptos::logging::log!("{:?}", data.get());
         }>
