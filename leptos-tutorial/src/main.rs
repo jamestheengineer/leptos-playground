7e625e2c6f804a7bb770f8f565f82a2afdf5ec21
jamestheengineer <jamestheengineer@gmail.com>
2025-04-04 17:52:26 -0400

    some aync loading of resources

--- leptos-tutorial/src/main.rs
+++ leptos-tutorial/src/main.rs
@@ -1,56 +1,51 @@
 use gloo_timers::future::TimeoutFuture;
 use leptos::prelude::*;
 
-// Here we define an async function
-// This could be anything: a network request, database read, etc.
-// Here, we just multiply a number by 10
-async fn load_data(value: i32) -> i32 {
-    // fake a one-second delay
+async fn important_api_call(name: String) -> String {
     TimeoutFuture::new(1_000).await;
-    value * 10
+    name.to_ascii_uppercase()
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    // this count is our synchronous, local state
-    let (count, set_count) = signal(0);
+    let (name, set_name) = signal("Bill".to_string());
 
-    // tracks `count`, and reloads by calling `load_data`
-    // whenever it changes
-    let async_data = LocalResource::new(move || load_data(count.get()));
-
-    // a resource will only load once if it doesn't read any reactive data
-    let stable = LocalResource::new(|| load_data(1));
-
-    // we can access the resource values with .get()
-    // this will reactively return None before the Future has resolved
-    // and update to Some(T) when it has resolved
-    let async_result = move || {
-        async_data
-            .get()
-            .as_deref()
-            .map(|value| format!("Server returned {value:?}"))
-            // This loading state will only show before the first load
-            .unwrap_or_else(|| "Loading...".into())
-    };
+    // this will reload every time `name` changes
+    let async_data = LocalResource::new(move || important_api_call(name.get()));
 
     view! {
-        <button
-            on:click=move |_| *set_count.write() += 1
+        <input
+            on:change:target=move |ev| {
+                set_name.set(ev.target().value());
+            }
+            prop:value=name
+        />
+        <p><code>"name:"</code> {name}</p>
+        <Suspense
+            // the fallback will show whenever a resource
+            // read "under" the suspense is loading
+            fallback=move || view! { <p>"Loading..."</p> }
         >
-            "Click me"
-        </button>
-        <p>
-            <code>"stable"</code>": " {move || stable.get().as_deref().copied()}
-        </p>
-        <p>
-            <code>"count"</code>": " {count}
-        </p>
-        <p>
-            <code>"async_value"</code>": "
-            {async_result}
-            <br/>
-        </p>
+            // Suspend allows you use to an async block in the view
+            <p>
+                "Your shouting name is "
+                {move || Suspend::new(async move {
+                    async_data.await
+                })}
+            </p>
+        </Suspense>
+        <Suspense
+            // the fallback will show whenever a resource
+            // read "under" the suspense is loading
+            fallback=move || view! { <p>"Loading..."</p> }
+        >
+            // the children will be rendered once initially,
+            // and then whenever any resources has been resolved
+            <p>
+                "Which should be the same as... "
+                {move || async_data.get().as_deref().map(ToString::to_string)}
+            </p>
+        </Suspense>
     }
 }
 
